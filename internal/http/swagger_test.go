7ea07429da6c1af604904a2479ce3cf11b5e41d9
Aibolat <aibolatermekbay@internet.ru>
2026-02-05 20:17:15 +0500

    test: ensure swagger route is available

---
+++ internal/http/swagger_test.go
@@ -0,0 +1,21 @@
+package httpx_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	httpx "idsai-core-up/internal/http"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestSwaggerRoute_Available(t *testing.T) {
+	r := httpx.NewRouter(nil)
+
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/swagger/index.html", nil)
+	r.ServeHTTP(w, req)
+
+	require.Equal(t, http.StatusOK, w.Code)
+}
